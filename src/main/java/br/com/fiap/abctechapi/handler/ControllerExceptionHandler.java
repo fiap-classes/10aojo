74e6ed4c4ecc47807c97ff76f31847ce97c06cc8
Luiz Zenha <lzenha@gmail.com>
2023-01-18 21:48:58 -0300

    Validations + Error Handler

--- src/main/java/br/com/fiap/abctechapi/handler/ControllerExceptionHandler.java
+++ src/main/java/br/com/fiap/abctechapi/handler/ControllerExceptionHandler.java
@@ -4,9 +4,11 @@ import br.com.fiap.abctechapi.handler.exception.MaxAssistsException;
 import br.com.fiap.abctechapi.handler.exception.MinimumAssistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ValidationException;
 import java.util.Date;
 
 @ControllerAdvice
@@ -34,5 +36,18 @@ public class ControllerExceptionHandler {
         );
 
         return new ResponseEntity<ErrorMessageResponse>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessageResponse> validationException(MethodArgumentNotValidException exception){
+        ErrorMessageResponse error = new ErrorMessageResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                new Date(),
+                "Validation error",
+                exception.getAllErrors().get(0).getDefaultMessage()
+        );
+
+        return new ResponseEntity<ErrorMessageResponse>(error, HttpStatus.BAD_REQUEST);
+
     }
 }

cc01a35f53db7b45d17cf1ec0faeff461e42c5ca
Luiz Zenha <lzenha@gmail.com>
2023-01-18 21:36:52 -0300

    Flyway

--- src/main/java/br/com/fiap/abctechapi/service/impl/OrderServiceImpl.java
+++ src/main/java/br/com/fiap/abctechapi/service/impl/OrderServiceImpl.java
@@ -8,10 +8,12 @@ import br.com.fiap.abctechapi.repository.AssistRepository;
 import br.com.fiap.abctechapi.repository.OrderRepository;
 import br.com.fiap.abctechapi.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class OrderServiceImpl implements OrderService {
 
     private OrderRepository orderRepository;
